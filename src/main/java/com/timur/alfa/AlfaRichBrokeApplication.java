46f216a59ef966ea2fb321227644e4a31629731d
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-06-02 21:33:37 +0300

    initial setup of project

---
+++ src/main/java/com/timur/alfa/AlfaRichBrokeApplication.java
@@ -0,0 +1,13 @@
+package com.timur.alfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaRichBrokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaRichBrokeApplication.class, args);
+	}
+
+}
